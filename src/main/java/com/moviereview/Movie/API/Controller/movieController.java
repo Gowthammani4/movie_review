fb8515e544a6fb397d42567cbe1e1c0182b1ad7b
Gowthammani4 <gowthammani7065@gmail.com>
2023-12-26 16:50:31 +0530

    update endpoint

--- src/main/java/com/moviereview/Movie/API/Controller/movieController.java
+++ src/main/java/com/moviereview/Movie/API/Controller/movieController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/movie")
+@RequestMapping("/api/movies")
 public class movieController {
     @Autowired
     private movieService movieservice;
