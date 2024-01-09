8b36a5f3ffa3e37c85c03ee11530fba62b1b4ebe
Gowthammani4 <gowthammani7065@gmail.com>
2024-01-09 22:06:55 +0530

    login page update

--- src/main/java/com/moviereview/Movie/API/repository/UserRepository.java
+++ src/main/java/com/moviereview/Movie/API/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.moviereview.Movie.API.repository;
 
+import com.moviereview.Movie.API.model.UserDetails;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<String,String> {
-
+public interface UserRepository extends MongoRepository<UserDetails, ObjectId> {
+UserDetails findByEmailIgnoreCase(String email);
+Boolean existsByEmail(String email);
 }
