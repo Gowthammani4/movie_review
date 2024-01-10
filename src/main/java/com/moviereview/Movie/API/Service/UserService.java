314371829b7d135328d7bb5258214908155b057b
Gowthammani4 <gowthammani7065@gmail.com>
2024-01-10 22:15:49 +0530

    update user details

--- src/main/java/com/moviereview/Movie/API/Service/UserService.java
+++ src/main/java/com/moviereview/Movie/API/Service/UserService.java
@@ -2,6 +2,7 @@ package com.moviereview.Movie.API.Service;
 
 import com.moviereview.Movie.API.model.UserDetails;
 import com.moviereview.Movie.API.repository.UserRepository;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
@@ -18,16 +19,18 @@ public class UserService {
     public void saveUser(UserDetails userDetails){
         if(userRepository.existsByEmail(userDetails.getEmail())){
             return;}
-        userRepository.save(new UserDetails(userDetails.getUserName(),userDetails.getEmail(),userDetails.getPassword()));;
+        UserDetails newData =new UserDetails(userDetails.getUserName(),userDetails.getEmail(),userDetails.getPassword());
+        newData.setConfirmationToken(newData.getId());
+        userRepository.save(newData);
         userDetails.setConfirmationToken(userDetails.getId());
         SimpleMailMessage mailMessage=new SimpleMailMessage();
         mailMessage.setTo(userDetails.getEmail());
         mailMessage.setSubject("Complete Registration!");
         mailMessage.setText("To confirm your account, please click here : "
-                +"http://localhost:8085/confirm-account?token="+userDetails.getConfirmationToken());
+                +"http://localhost:8085/confirm-account?token="+userDetails.getId());
         emailService.sendMail(mailMessage);
 
-        System.out.println("Confirmation Token: " + userDetails.getConfirmationToken());
+        System.out.println("Confirmation Token: " + userDetails.getId());
 
 
     }
